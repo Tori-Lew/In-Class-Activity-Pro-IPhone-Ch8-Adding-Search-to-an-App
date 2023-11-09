b16489c8f934db24580c1ee2a764b9f8ef873526
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-08 16:15:25 -0800

    Initial Commit

---
+++ In-Class Activity Pro IPhone Ch8 Adding Search to an App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch8 Adding Search to an App
+//
+//  Created by Student Account on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch8 Adding Search to an App/In_Class_Activity_Pro_IPhone_Ch8_Adding_Search_to_an_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch8_Adding_Search_to_an_AppApp.swift
+//  In-Class Activity Pro IPhone Ch8 Adding Search to an App
+//
+//  Created by Student Account on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch8_Adding_Search_to_an_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
